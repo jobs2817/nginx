46039576e43ad9d8e3e74beb67c9f41c9f978c4c
v_fuhaofei <2104647166@qq.com>
2020-04-07 00:34:39 +0800

    test

--- src/router.js
+++ src/router.js
@@ -3,10 +3,10 @@ import router from 'vue-router'
 Vue.use(router)
 import a from './components/a.vue'
 import b from './components/b.vue'
-
+console.log(process.env.BASE_URL, 'process.env.BASE_URL')
 export default new router({
   // mode: 'history',
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
