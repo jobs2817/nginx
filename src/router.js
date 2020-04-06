e6764f2b18854f2f9eeb3cf44e5b15bc761fd4ca
v_fuhaofei <2104647166@qq.com>
2020-04-07 00:17:12 +0800

    process.env.BASE_URL

--- src/router.js
+++ src/router.js
@@ -4,9 +4,10 @@ Vue.use(router)
 import a from './components/a.vue'
 import b from './components/b.vue'
 
-
+console.info(process.env, 'process.env.PUBLIC_PAYH,')
 export default new router({
   mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
